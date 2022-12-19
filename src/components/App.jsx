10d0b463bea2baa4ded6801bd4a21caa4fa4114a
Roman Denysenko <97247092+Romannorish@users.noreply.github.com>
2022-12-19 22:59:59 +0300

    commit user

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,16 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import Profile from "./Profile/Profile";
+import user from 'data/user'
+
+
+export default function App() {
+  return <div className="App">
+    <Profile 
+            avatar={user.avatar}
+            username={user.username}
+            tag={user.tag}
+            location={user.location}
+            stats={user.stats}
+        />
+  </div>
+
+}
